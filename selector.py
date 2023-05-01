fd35b157354bf6e9fa72e38ecbfcd82328346d19
cJinu <104177326+cJinu@users.noreply.github.com>
2023-05-01 14:35:28 +0900

    feat: select 6 nums in range 1 to 45

--- selector.py
+++ selector.py
@@ -1,6 +1,9 @@
-import random.random
+import random
 
 def _select():
-    return 'hello'
+    A = set([])
+    while len(A)<6:
+        A.add(random.randint(1,45))
+    return A
 
 print(_select())
